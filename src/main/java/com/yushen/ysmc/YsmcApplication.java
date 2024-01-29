19841adab01fa2d3d34b089e22cb494940e38a48
yushen0 <820674392@qq.com>
2024-01-29 11:10:11 +0800

    init project

---
+++ src/main/java/com/yushen/ysmc/YsmcApplication.java
@@ -0,0 +1,13 @@
+package com.yushen.ysmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YsmcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yushen/ysmc/YsmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yushen.ysmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
