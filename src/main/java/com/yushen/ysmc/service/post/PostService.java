62b97072e62fa2f7065db265bb397704209e8955
yushen0 <820674392@qq.com>
2025-02-18 15:28:54 +0800

    feat:方法补充

--- src/main/java/com/yushen/ysmc/service/post/PostService.java
+++ src/main/java/com/yushen/ysmc/service/post/PostService.java
@@ -1,7 +1,45 @@
 package com.yushen.ysmc.service.post;
 
+import com.yushen.ysmc.controller.post.vo.PostBaseRespVO;
+import com.yushen.ysmc.controller.post.vo.PostCreateReqVO;
+
+import java.util.List;
+
 /**
  * @author: yushen
  **/
 public interface PostService {
+
+    /**
+     * 创建帖子
+     * @param reqVO
+     */
+    void createPost(PostCreateReqVO reqVO);
+
+    /**
+     * 更新帖子
+     * @param reqVO
+     */
+    void updatePost(PostCreateReqVO reqVO);
+
+    /**
+     * 删除帖子
+     * @param id
+     */
+    void deletePost(Long id);
+
+    /**
+     * 获取帖子
+     * @param id
+     * @return
+     */
+    PostBaseRespVO getPost(Long id);
+
+    /**
+     * 获取帖子列表
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    List<PostBaseRespVO> getPostList(Integer pageNum, Integer pageSize);
 }
